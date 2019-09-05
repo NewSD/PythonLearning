4d7cdc1c11b585f5816b09485e403ac1de3a55c7
18300603955 <oceaniu@163.com>
2019-09-05 16:41:48 +0800

    bytearray

--- ch4/datatype.py
+++ ch4/datatype.py
@@ -2,20 +2,20 @@ import math, random
 
 num = 123 + 456
 print(num)
-num2=1.5*4
+num2 = 1.5 * 4
 print(num2)
-num3=2**100
+num3 = 2 ** 100
 print(num3)
 # l = len(str(2**1000000))
 # print(l)
 a = 3.1415 * 2
 print(a)
-print(3.1415*2)
+print(3.1415 * 2)
 
 print(math.pi)
 print(math.sqrt(85))
 print(random.random())
-print(random.choice([1,2,3,4]))
+print(random.choice([1, 2, 3, 4]))
 
 S = 'Spam'
 print(len(S))
@@ -23,7 +23,7 @@ print(S[0])
 print(S[1])
 print(S[-1])
 print(S[-2])
-print(S[len(S)-1])
+print(S[len(S) - 1])
 print(S)
 print(S[1:3])
 print(S[0:3])
@@ -31,5 +31,18 @@ print(S[:3])
 print(S[:-1])
 print(S[0:-1])
 print(S[:])
-
-
+print(S + 'xyz')
+print(S * 8)
+# TypeError: 'str' object does not support item assignment
+# S[0] = 'z'
+S = 'z' + S[1:]
+print(S)
+S = 'shrubbery'
+L = list(S)
+print(L)
+L[1]='c'
+print(''.join(L))
+B = bytearray(b'spam')
+B.extend(b'eggs')
+print(B)
+print(B.decode())
