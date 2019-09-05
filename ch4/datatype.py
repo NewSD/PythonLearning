5162a000cb0cc6fafd47cb62d5fb52ca46c1266f
18300603955 <oceaniu@163.com>
2019-09-05 16:33:59 +0800

    S[:]

--- ch4/datatype.py
+++ ch4/datatype.py
@@ -16,3 +16,20 @@ print(math.pi)
 print(math.sqrt(85))
 print(random.random())
 print(random.choice([1,2,3,4]))
+
+S = 'Spam'
+print(len(S))
+print(S[0])
+print(S[1])
+print(S[-1])
+print(S[-2])
+print(S[len(S)-1])
+print(S)
+print(S[1:3])
+print(S[0:3])
+print(S[:3])
+print(S[:-1])
+print(S[0:-1])
+print(S[:])
+
+
