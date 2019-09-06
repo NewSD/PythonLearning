8327ca1bc3ce9834ef31c4d67d9ae591a53ab725
18300603955 <oceaniu@163.com>
2019-09-06 16:33:27 +0800

    list test

---
+++ ch4/ListTest.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+@version: ??
+@author: ami
+@license: Apache Licence 
+@file: ListTest.py
+@time: 2019/9/6 16:25
+@tools: PyCharm
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
+L = [123, 'spam', 1.23]
+print(L)
+print(len(L))
+print(L[0])
+print(L[:-1])
+print(L + [4, 5, 6])
+print(L * 2)
+print(L)
