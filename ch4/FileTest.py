b8e093ed9ffe1c1b67c7ee84d54fecac514d2434
18300603955 <oceaniu@163.com>
2019-09-24 13:55:46 +0800

    write

---
+++ ch4/FileTest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+@version: ??
+@author: ami
+@license: Apache Licence 
+@file: FileTest.py
+@time: 2019/9/24 13:54
+@tools: PyCharm
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
+f = open('data.txt','w')
+f.write('Hello\n')
+f.write('hello\n')
+f.close()
