7886e380fde4676628442576cb63426932ff6fca
18300603955 <oceaniu@163.com>
2019-09-24 14:15:33 +0800

    decode

--- ch4/FileTest.py
+++ ch4/FileTest.py
@@ -38,12 +38,41 @@ for line in open('data.txt'): print(line)
 print(dir(f))
 print(help(f.seek))
 
-
-
 import struct
-packed = struct.pack('>i4sh',7,b'spam',8)
+
+packed = struct.pack('>i4sh', 7, b'spam', 8)
 print(packed)
 
-file = open('data.bin','wb')
+file = open('data.bin', 'wb')
 file.write(packed)
 file.close()
+
+data = open('data.bin', 'rb').read()
+print(data)
+print(data[4:8])
+print(list(data))
+print(struct.unpack('>i4sh', data))
+
+# Unicode
+
+S = 'sp\xc4m'
+print(S)
+print(S[2])
+
+file = open('unidata.txt', 'w', encoding='utf-8')
+file.write(S)
+file.close()
+
+text = open('unidata.txt', encoding='utf-8').read()
+print(text)
+print(len(text))
+raw = open('unidata.txt', 'rb').read()
+print(raw)
+print(raw.decode('utf-8'))
+
+
+print(text.encode('latin-1'))
+print(text.encode('utf-16'))
+print(len(text.encode('latin-1')),len(text.encode('utf-16')))
+
+print(b'\xff\xfes\x00p\x00\xc4\x00m\x00'.decode('utf-16'))
