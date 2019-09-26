5390f70119c7211cb8587a4df809f2c39b68005e
18300603955 <oceaniu@163.com>
2019-09-26 13:37:40 +0800

    L

--- ch9/fileTest.py
+++ ch9/fileTest.py
@@ -132,3 +132,15 @@ try:
         print(line)
 finally:
     myfile.close()
+
+
+L = ['abc',[(1,2),([3],4)],5]
+
+print(L[0])
+print(L[1])
+print(L[1][0])
+print(L[1][1])
+print(L[1][1][0])
+print(L[1][1][1])
+print(L[1][1][0][0])
+
