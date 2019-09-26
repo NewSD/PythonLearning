a61be5e95c5d5e2868c3b2754daa02450c40bbf9
18300603955 <oceaniu@163.com>
2019-09-26 13:43:19 +0800

    while

---
+++ ch10/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+@version: ??
+@author: ami
+@license: Apache Licence 
+@file: __init__.py.py
+@time: 2019/9/26 13:37
+@tools: PyCharm
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ ch10/sentenceTest.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+@version: ??
+@author: ami
+@license: Apache Licence 
+@file: sentenceTest.py
+@time: 2019/9/26 13:39
+@tools: PyCharm
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
+x = 1
+y = 2
+if x > y:
+    print('x>y')
+else:
+    print('x<=y')
+
+
+a = 1; b = 2; print(a + b)
+
+# while True:
+#     reply = input("Enter text:")
+#     if reply == 'stop': break
+#     print(reply.upper())
+
+
+
