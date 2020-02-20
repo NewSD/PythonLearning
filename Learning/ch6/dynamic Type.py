145a993802b2d5dd2e3f40ee305a058676e9830f
NiuHaiDeng <niuhaideng@163.com>
2020-02-20 15:55:49 +0800

    dd

---
+++ Learning/ch6/dynamic Type.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+@version: ??
+@author: ami
+@license: Apache Licence 
+@file: dynamic Type.py
+@time: 2020/2/20 15:49
+@tools: PyCharm
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
